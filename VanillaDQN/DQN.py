da6db68f6aadc2855ab3333f94c32a1c06045d4d
Dylan-Trollope <dylan.trollope@gmail.com>
2022-06-22 11:37:31 +0200

    Initial complete implementation of VDQN and ConvDQN

--- VanillaDQN/DQN.py
+++ VanillaDQN/DQN.py
@@ -19,12 +19,48 @@ class DQN(nn.Module):
                 nn.Linear(256, self.output_dim)
         )
 
-
     def forward(self, state):
-        q_vals = self.cn(state)
-        return q_vals
+        vals = self.cn(state)
+        return vals
+
+
+class ConvolutionalDQN(nn.Module, in_dims, out_dims):
+	
+	def __init__(self, in_dims, out_dims):
+		super(ConvolutionalDQN, self).__init__()
+		self.in_dims = in_dims
+		self.out_dims = out_dims
+		self.features = self.feature_dims()
+
+		self.conv_net = nn.Sequential(
+				nn.Conv2d(self.in_dims, 32, kernel_size=8, stride=4),
+				nn.ReLu(),
+				nn.Conv2d(32, 64, kernel_size=4, stride=2),
+				nn.ReLu(),
+				nn.Conv2d(64, 64, kernel_size=3, stride=1),
+				nn.ReLu()
+		)
+
+		self.feature_net = nn.Sequential(
+				self.Linear(self.features, 128),
+				self.ReLU(),
+				self.Linear(128, 256),
+				nn.ReLU(),
+				nn.Linear(256, self.out_dims)
+		)
 
 
 
- # Need to do COnvDQN over here
+	def forward(self, state):
+		features = self.conv_net(state)
+		features = features.view(features.size(0), -1)
+		q_vals = self.feature_net(features)
+		return q_vals
+		
+
+	
+	def feature_dims():
+		return self.conv_net(autograd.Variable(torch.zeros(1, *self.in_dims))).view(1, -1).size(1)
+		
+
 
